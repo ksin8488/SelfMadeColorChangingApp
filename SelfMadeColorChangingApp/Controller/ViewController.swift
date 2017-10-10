6fdb4e13a7a3e80fde35ab19d470767b934247fb
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-10-10 12:42:59 -0600

    Edited color randomizer to show how you can use random in multiple different ways.

--- SelfMadeColorChangingApp/Controller/ViewController.swift
+++ SelfMadeColorChangingApp/Controller/ViewController.swift
@@ -29,8 +29,8 @@ class ViewController: UIViewController {
     {
         let newColor :UIColor
         let redValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
-        let greenValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
-        let blueValue :CGFloat = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let greenValue :CGFloat = CGFloat (Double (arc4random() % 256) / 255.00)
+        let blueValue :CGFloat = CGFloat (drand48())
         newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1))
         
         return newColor
