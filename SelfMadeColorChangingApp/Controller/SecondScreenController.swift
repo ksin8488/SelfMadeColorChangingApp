611fe02f7f905a40a56701c5490bb015f70a1d99
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-10-12 13:19:05 -0600

    Added a second screen to the storyboard and a controlfor it. Added segway between screens in the app.

---
+++ SelfMadeColorChangingApp/Controller/SecondScreenController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondScreenController.swift
+//  SelfMadeColorChangingApp
+//
+//  Created by Singh, Kashish on 10/12/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+ import UIKit
+
+import Foundation
+
+public class SecondScreenController : UIViewController
+{
+    
+}
