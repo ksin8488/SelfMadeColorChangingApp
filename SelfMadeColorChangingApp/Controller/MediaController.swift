6749b5459b2a82fb6f281861b9ae2adc6afd89e5
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-10-18 13:21:15 -0600

    Added 2 Buttons, a slider, and an image frame as well as add coding for the 3 images that were added to display on the fram with a click of the button.

--- SelfMadeColorChangingApp/Controller/MediaController.swift
+++ SelfMadeColorChangingApp/Controller/MediaController.swift
@@ -12,10 +12,57 @@ import UIKit
 public class MediaController : UIViewController
 {
     private lazy var color: ColorTools = ColorTools() //formal way
+    private var imageCounter : Int = 0
     
     public override func viewDidLoad()
     {
         super.viewDidLoad() //automatically gets called as soon as you go to this screen
         view.backgroundColor = color.createRandomColor()
     }
+    
+
+    @IBOutlet weak var soundButton: UIButton!
+    @IBOutlet weak var soundSlider: UISlider!
+    @IBOutlet weak var imageButton: UIButton!
+    @IBOutlet weak var imageFrame: UIImageView!
+    
+    @IBAction func soundButtonMethod(_ sender: UIButton)
+    {
+    
+    }
+    
+    @IBAction func soundSliderMethod(_ sender: UISlider)
+    {
+        
+    }
+    
+    @IBAction func imageButtonMethod(_ sender: UIButton)
+    {
+        changeImage()
+        view.backgroundColor = color.createRandomColor()
+    }
+    
+    private func changeImage() -> Void
+    {
+        if (imageCounter > 2)
+        {
+            imageCounter = 0
+        }
+        
+        if (imageCounter == 0)
+        {
+            imageFrame.image = UIImage(named: "DoingItWrong")
+        }
+        else if (imageCounter == 1)
+        {
+            imageFrame.image = UIImage(named: "DontUnderstandPoster")
+        }
+        else
+        {
+            imageFrame.image = UIImage(named: "HowDoesItWork")
+        }
+        
+        imageCounter += 1
+    }
+    
 }
